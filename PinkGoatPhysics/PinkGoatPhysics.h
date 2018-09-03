cb6e881e97b9d3fe3fc53e84bcd0b6ff6a9d197f
Roy Li <roy.li.zixuan@gmail.com>
2018-09-03 09:40:33 -0700

    Initial Commit

---
+++ PinkGoatPhysics/PinkGoatPhysics.h
@@ -0,0 +1,19 @@
+//
+//  PinkGoatPhysics.h
+//  PinkGoatPhysics
+//
+//  Created by Roy Li on 9/3/18.
+//  Copyright © 2018 Roy Li. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for PinkGoatPhysics.
+FOUNDATION_EXPORT double PinkGoatPhysicsVersionNumber;
+
+//! Project version string for PinkGoatPhysics.
+FOUNDATION_EXPORT const unsigned char PinkGoatPhysicsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PinkGoatPhysics/PublicHeader.h>
+
+
